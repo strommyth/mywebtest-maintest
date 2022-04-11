0dd873fca6775feabfb99b88eaffe0ecb9d9911a
strommyth <okko6543212@gmail.com>
2022-04-11 12:24:28 +0800

    kakeru

---
+++ elements.php
@@ -0,0 +1,276 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+
+		<title>東大資工面試報到</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body>
+		<?php
+			require("conn_mysql.php");
+		?>
+		<!-- Header -->
+			<header id="header">
+				<nav class="left">
+					<a href="#menu"><span>目錄</span></a>
+				</nav>
+				<a href="index.php" class="logo">National Taitung University</a>
+				<nav class="right">
+					<?php
+						header("Content-Type:text/html;charset=utf-8");
+						session_start();
+						if(isset($_COOKIE['Uname']))
+						{
+							//已經登入
+							if($_COOKIE['Ulogin']=="管理員")
+								echo "<a href='logout.php' class='button alt'>Log Out</a>";
+							else
+								echo"<script language=\"JavaScript\">alert('當前帳號無權訪問此頁面');location.href=\"index.php\";</script>";
+						}
+						else
+						{
+							echo"<script language=\"JavaScript\">alert('請先登入');location.href=\"loginpage.php\";</script>";
+						}
+					?>
+				</nav>
+			</header>
+
+		<!-- Menu -->
+			<nav id="menu">
+				<ul class="links">
+					<li><a href="index.php">首頁</a></li>
+					<li><a href="survey.php">問卷作答(考生使用)</a></li>
+					<?php
+						if($_COOKIE['Ulogin']=='管理員'||$_COOKIE['Ulogin']=='工作人員')
+						{
+					?>
+						<li><a href="checkin.php">報到狀態(報到處使用)</a></li>
+						<li><a href="status_302.php">填答狀態(C302使用)</a></li>
+						<li><a href="status_311.php">考生狀態(C311使用)</a></li>
+						<li><a href="status_5F.php">考生狀態(420使用)</a></li>
+						<li><a href="status_420.php">考生狀態(420使用)</a></li>
+						<li><a href="status_403.php">考生狀態(403使用)</a></li>
+					<?php
+						}
+					?>
+					<?php
+						if($_COOKIE['Ulogin']=='管理員')
+						{
+					?>
+							<li><a href="elements.php">資料顯示(考場內使用)</a></li>
+					<?php
+						}
+					?>
+				</ul>
+				<ul class="actions vertical">
+					<?php
+						header("Content-Type:text/html;charset=utf-8");
+						if(isset($_COOKIE['Uname']))
+						{
+							//已經登入
+							echo "<li><a href='logout.php' class='button fit'>Log out</a></li>";
+						}
+						else
+						{
+							echo "<li><a href='loginpage.php' class='button fit'>Login</a></li>";
+						}
+					?>
+				</ul>
+			</nav>
+
+		<!-- Two -->
+			<section id="two" class="wrapper  special">
+				<div class="inner" style="max-width:90%">
+					<h2>資料顯示(考場內使用)</h2>
+					<h3>請等待<font style="background:#9BF6FF">"藍格"</font>的人的到來</h3>
+					<h3>面試完畢後請點選按鈕確認變成<font style="background:#A0C4FF">"靛格"</font></h3>
+						<div class="12u">
+							<div class="table-wrapper" style="color:black;width:100%;">
+								<form method="post" style="width:100%;" action="">
+									<?php
+										if(isset($_POST['RefreshData']))
+										{
+											$SQL_DATA="SELECT * FROM `Candidate` WHERE `Class`=\"".$_POST['class']."\"and`Time`=\"".$_POST['time']."\"and`round`=\"".$_POST['round']."\" ORDER BY `Candidate`.`Time` ASC, `Candidate`.`round` ASC , `Candidate`.`Number` ASC";
+											$DATA_result_status=mysqli_query($db_link,$SQL_DATA) or die("查詢失敗");
+										}
+									?>
+									<div class="select-wrapper">
+										<select name="class"style="width:30%;float:left">
+											<option value="T"<?php if($_POST['class']=="T") echo "selected"?>>T</option>
+										</select>
+										<select name="time"style="width:30%;float:left">
+
+											<option value="01"<?php if($_POST['time']=="01") echo "selected"?>>1</option>
+											<option value="02"<?php if($_POST['time']=="02") echo "selected"?>>2</option>
+											<option value="03"<?php if($_POST['time']=="03") echo "selected"?>>3</option>
+											<option value="04"<?php if($_POST['time']=="04") echo "selected"?>>4</option>
+											<option value="05"<?php if($_POST['time']=="05") echo "selected"?>>5</option>
+											<option value="06"<?php if($_POST['time']=="06") echo "selected"?>>6</option>
+											<option value="07"<?php if($_POST['time']=="07") echo "selected"?>>7</option>
+											<option value="08"<?php if($_POST['time']=="08") echo "selected"?>>8</option>
+											<option value="09"<?php if($_POST['time']=="09") echo "selected"?>>9</option>
+											<option value="10"<?php if($_POST['time']=="10") echo "selected"?>>10</option>
+											<option value="11"<?php if($_POST['time']=="11") echo "selected"?>>11</option>
+										</select>
+										<select name="round"style="width:30%;float:left">
+											<option value="1"<?php if($_POST['round']=="1") echo "selected"?>>1</option>
+											<option value="2"<?php if($_POST['round']=="2") echo "selected"?>>2</option>
+											<option value="3"<?php if($_POST['round']=="3") echo "selected"?>>3</option>
+											<option value="4"<?php if($_POST['round']=="4") echo "selected"?>>4</option>
+											<option value="5"<?php if($_POST['round']=="5") echo "selected"?>>5</option>
+											<option value="6"<?php if($_POST['round']=="6") echo "selected"?>>6</option>
+											<option value="7"<?php if($_POST['round']=="7") echo "selected"?>>7</option>
+											<option value="8"<?php if($_POST['round']=="8") echo "selected"?>>8</option>
+											<option value="9"<?php if($_POST['round']=="9") echo "selected"?>>9</option>
+											<option value="10"<?php if($_POST['round']=="10") echo "selected"?>>10</option>
+											<option value="11"<?php if($_POST['round']=="11") echo "selected"?>>11</option>
+											<option value="12"<?php if($_POST['round']=="12") echo "selected"?>>12</option>
+											<option value="13"<?php if($_POST['round']=="13") echo "selected"?>>13</option>
+											<option value="14"<?php if($_POST['round']=="14") echo "selected"?>>14</option>
+											<option value="15"<?php if($_POST['round']=="15") echo "selected"?>>15</option>
+										</select>
+										<button class="button special small" style="float:left;width:10%;padding:0 0.3em" name="RefreshData">查詢</button>
+									</div>
+									</form>
+								<form method="post" style="width:100%;" action="elementssent.php">
+									<table class="alt" >
+										<thead>
+											<tr style="height:20px;">
+												<th style="text-align: center;width:5%;"><div style="overflow:auto;width:100%;height:100px;">准考證號</div></th>
+												<th style="text-align: center;width:6%;"><div style="overflow:auto;width:100%;height:100px;">考生<br>姓名</div></th>
+												<th style="text-align: center;width:6%;"><div style="overflow:auto;width:100%;height:100px;">梯次與場次</div></th>
+												<th style="text-align: center;width:10%;"><div style="overflow:auto;width:100%;height:100px;">考生狀態</div></th>
+												<th style="text-align: center;width:12%;"><div style="overflow:auto;width:100%;height:100px;">題目一</div></th>
+												<th style="text-align: center;width:11%;"><div style="overflow:auto;width:100%;height:100px;">題目二<br>假設您現在已經取得資訊工程學位，您想利用資訊科技來改善甚麼?怎麼做?</div></th>
+												<th style="text-align: center;width:11%;"><div style="overflow:auto;width:100%;height:100px;">題目三<br>最近令您印象深刻的資訊科技相關新聞是甚麼?為甚麼?</div></th>
+												<th style="text-align: center;width:11%;"><div style="overflow:auto;width:100%;height:100px;">題目四<br>您認為就讀資訊工程的學生，需要具備怎麼樣的能力或特質呢?</div></th>
+												<th style="text-align: center;width:11%;"><div style="overflow:auto;width:100%;height:100px;">題目五</div></th>
+												<th style="text-align: center;width:7%;"><div style="overflow:auto;width:100%;height:100px;">分數</div></th>
+												<th style="text-align: center;width:11%;"><div style="overflow:auto;width:100%;height:100px;">該生註記</div></th>
+											</tr>
+										</thead>
+										<tbody>
+										<?php
+											while ($DATA_rowres = mysqli_fetch_array($DATA_result_status, MYSQLI_BOTH))
+											{
+												if($DATA_rowres[6]=="302填寫中")
+													$Color="#FFADAD";
+												else if($DATA_rowres[6]=="前往311...")
+													$Color="#FFD6A5";
+												else if($DATA_rowres[6]=="402等待面試中")
+													$Color="#FDFFB6";
+												else if($DATA_rowres[6]=="抵達420")
+													$Color="#CAFFBF";
+												else if($DATA_rowres[6]=="開始面試")
+													$Color="#9BF6FF";
+												else if($DATA_rowres[6]=="回饋填寫中")
+													$Color="#A0C4FF";
+												else if($DATA_rowres[6]=="面試完畢")
+													$Color="#FFC6FF";
+												else//他還沒來
+													$Color="#F0EFEB";
+												?>
+												<tr>
+													<td style="background-color:<?php echo $Color?>"><?php echo $DATA_rowres[0]?></td>
+													<td style="background-color:<?php echo $Color?>"><?php echo $DATA_rowres[3]?></td>
+													<td style="background-color:<?php echo $Color?>"><?php echo $DATA_rowres[4]."-".$DATA_rowres[5]."-".$DATA_rowres[7]."(".$DATA_rowres[5].")"?></td>
+													<td style="background-color:<?php echo $Color?>"><?php echo $DATA_rowres[6]?></td>
+												<?php
+													if($DATA_rowres[8]=="1")
+													{
+												?>
+												<td class="zoom"style="background-color:<?php echo $Color?>"><?php echo "軟體設計與應用<br>".$DATA_rowres[9] ?></td>
+												<?php
+													}
+													else if($DATA_rowres[8]=="2")
+													{
+												?>
+												<td class="zoom"style="background-color:<?php echo $Color?>"><?php echo "嵌入式系統與應用<br>".$DATA_rowres[9] ?></td>
+												<?php
+													}
+													else if($DATA_rowres[8]=="3")
+													{
+												?>
+												<td class="zoom"style="background-color:<?php echo $Color?>"><?php echo "網路通訊與應用<br>".$DATA_rowres[9]?></td><?php
+													}
+													else
+													{
+														?>
+															<td style="background-color:<?php echo $Color?>"></td>
+
+													<?php
+													}
+													if($DATA_rowres[10]!='')
+													{
+													?>
+													<td class="zoom"style="background-color:<?php echo $Color?>"><?php echo $DATA_rowres[10]?></td>
+													<?php
+														}
+													if($DATA_rowres[10]!='')
+														{
+													?>
+													<td class="zoom"style="background-color:<?php echo $Color?>"><?php echo $DATA_rowres[11] ?></td>
+													<?php
+														}
+													if($DATA_rowres[10]!='')
+														{
+													?>
+													<td class="zoom"style="background-color:<?php echo $Color?>"><?php echo $DATA_rowres[12] ?></td>
+													<?php
+														}
+													?>
+													<td class="zoom"style="background-color:<?php echo $Color?>"><?php echo $DATA_rowres[13] ?></td>
+
+													<td style="background-color:<?php echo $Color?>"><textarea name='<?php echo $DATA_rowres[0].'_score'?>'   placeholder='分數' rows='2' style=' resize: none;'><?php echo $DATA_rowres[14]?></textarea></td>
+													<td style="background-color:<?php echo $Color?>"><textarea name='<?php echo $DATA_rowres[0].'_stunotice'?>'  placeholder='考生註記' rows='6' style=' resize: none;'><?php echo $DATA_rowres[15]?></textarea></td>
+												</tr>
+											<?php
+											}?>
+										</tbody>
+									</table>
+									<div style="width:100%;">
+										<button class="button special small" style="float:center;width:10%;padding:0 0.3em" name="NewData">本梯次面試結束</button>
+										<input type="hidden" value='<?php echo $_POST['class']?>' name="class">
+										<input type="hidden" value='<?php echo $_POST['time']?>' name="time">
+										<input type="hidden" value='<?php echo $_POST['round']?>' name="round">
+									</div>
+								</form>
+
+									<div style="width:100%;">
+										<button class="button small" style="float:center;width:10%;padding:0 0.3em"  onclick="location.href='export.php'">匯出所有資料</button>
+									</div>
+
+
+
+							</div>
+						</div>
+				</div>
+			</section>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="inner">
+					<h2>聯絡我們</h2>
+					<ul class="actions">
+						<li><span class="icon fa-phone"></span> <a href="#">(089)318855#6201</a></li>
+						<li><span class="icon fa-envelope"></span> <a href="#"> chifu@nttu.edu.tw</a></li>
+						<li><span class="icon fa-map-marker"></span>國立臺東大學 資訊工程學系</li>
+					</ul>
+				</div>
+				<div class="copyright">
+					&copy; Untitled. Design 資工四甲10611128劉紀佑
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
---
+++ logincheck.php
@@ -0,0 +1,61 @@
+<?php
+	header("Content-Type:text/html;charset=utf-8");
+	session_start();
+	
+	if(isset($_POST['trylogin']))
+	{
+		require("conn_mysql.php");
+		$Uname = trim($_POST['Uname']);
+		$Upwd = trim($_POST['Upwd']);
+		$check="0";
+		$GroupID="-1";
+		$sql_query_login="SELECT * FROM administrator where account='$Uname'";
+		$result1=mysqli_query($db_link,$sql_query_login) or die("查詢失敗1");
+		while($row=mysqli_fetch_array($result1))
+		{
+			if($row[1]==$Upwd)
+			{
+				$check="1";
+				$Group=$row[2];
+				$sql_query_name="SELECT Name FROM Candidate where ExamID='$Uname'";
+				$resultName=mysqli_query($db_link,$sql_query_name) or die("查詢失敗2");
+				$rowName=mysqli_fetch_array($resultName);
+				$wname = $rowName[0];
+				if($Group=="考生")//偷存編號
+				{	
+					$sql_query_Group="SELECT Number FROM Candidate where ExamID='$Uname'";
+					$resultGroup=mysqli_query($db_link,$sql_query_Group) or die("查詢失敗3");
+					while($rowGroup=mysqli_fetch_array($resultGroup))
+					{
+						$GroupID=$rowGroup[0];
+						break;
+					}
+				}
+				break;
+			}
+		}
+		if(($Uname=='')||($Upwd==''))
+		{
+			echo"<script language=\"JavaScript\">alert('准考證號或身分證字號輸入錯誤');location.href=\"loginpage.php\";</script>";
+		}
+		else if(($check=="1"))
+		{
+			//登入成功將資訊儲存到session中
+			$_SESSION['Uname']=$Uname;
+			$_SESSION['Ulogin']=$Group;
+			$_SESSION['UloginID']=$GroupID;
+			$_SESSION['wname']=$wname;
+			setcookie("Uname",$Uname);
+			setcookie("Ulogin",$Group);
+			setcookie("GroupID",$GroupID);
+			setcookie("wname",$wname);
+			//跳轉到使用者首頁
+			header('refresh:0;url=index.php');
+		}
+		else
+		{
+			echo"<script language=\"JavaScript\">alert('准考證號或身分證字號輸入錯誤');location.href=\"loginpage.php\";</script>";
+		}
+	}
+	echo"<script language=\"JavaScript\">location.href=\"index.php\";</script>";
+?>
